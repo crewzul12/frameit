e32e2f5e3f4ee118fd6ea76f368510e4463c3e9f
heiz99 <haziqazfar041@gmail.com>
2020-07-27 19:18:59 +0800

    added codepush package

---
+++ Update.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import CodePush from "react-native-code-push";
+
+class Update extends Component {
+  constructor() {
+    super();
+    this.state = { restartAllowed: true };
+  }
+
+  codePushStatusDidChange(syncStatus) {
+    switch(syncStatus) {
+      case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+        this.setState({ syncMessage: "Checking for update." });
+        break;
+      case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+        this.setState({ syncMessage: "Downloading package." });
+        break;
+      case CodePush.SyncStatus.AWAITING_USER_ACTION:
+        this.setState({ syncMessage: "Awaiting user action." });
+        break;
+      case CodePush.SyncStatus.INSTALLING_UPDATE:
+        this.setState({ syncMessage: "Installing update." });
+        break;
+      case CodePush.SyncStatus.UP_TO_DATE:
+        this.setState({ syncMessage: "App up to date.", progress: false });
+        break;
+      case CodePush.SyncStatus.UPDATE_IGNORED:
+        this.setState({ syncMessage: "Update cancelled by user.", progress: false });
+        break;
+      case CodePush.SyncStatus.UPDATE_INSTALLED:
+        this.setState({ syncMessage: "Update installed and will be applied on restart.", progress: false });
+        break;
+      case CodePush.SyncStatus.UNKNOWN_ERROR:
+        this.setState({ syncMessage: "An unknown error occurred.", progress: false });
+        break;
+    }
+  }
+
+  /** Update is downloaded silently, and applied on restart (recommended) */
+  sync() {
+    CodePush.sync(
+      {},
+      this.codePushStatusDidChange.bind(this)
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.sync.bind(this)}>
+          <Text style={styles.syncButton}>Check for updates</Text>
+        </TouchableOpacity>
+        <Text style={styles.messages}>{this.state.syncMessage || ""}</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#F8FAFD",
+    paddingTop: 50
+  },
+  messages: {
+    marginTop: 5,
+    textAlign: "center",
+  },
+  syncButton: {
+    color: "green",
+    fontSize: 17
+  },
+});
+
+let codePushOptions = { 
+  updateDialog: true,
+  installMode: CodePush.InstallMode.IMMEDIATE,
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START 
+};
+
+Update = CodePush(codePushOptions)(Update);
+
+export default Update;
